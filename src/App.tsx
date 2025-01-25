85254fca3110f3e77937bcc36982672c9d1ff7f1
devemit <mitkoiliev505@gmail.com>
2025-01-25 19:26:49 +0100

    file fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router';
-import { AppWrapper } from './layout/app-layout';
-import { AuthProvider } from './supabase/providers/AuthProvider';
+import { AppWrapper } from './layouts/app-layout';
 
 import { MainContent } from './components/ui/main-content';
 import { Navbar } from './components/ui/navbar';
@@ -11,28 +10,26 @@ import { AboutUs } from './components/views/about-us';
 import { Home } from './components/views/home';
 import { Search } from './components/views/search';
 import { Profile } from './components/views/profile';
-import { AuthCallback } from './supabase/components/AuthCallBack';
+import { AuthCallback } from './supabase/providers/AuthCallBack';
 
 const App = () => {
    return (
-      <AuthProvider>
-         <AppWrapper>
-            <Navbar />
-            <div className='flex flex-col md:flex-row w-full pt-16'>
-               <Sidebar />
-               <MainContent>
-                  <Routes>
-                     <Route path='/' element={<Home />} />
-                     <Route path='/discover' element={<Discover />} />
-                     <Route path='/search' element={<Search />} />
-                     <Route path='/about-us' element={<AboutUs />} />
-                     <Route path='/profile' element={<Profile />} />
-                     <Route path='/auth/callback' element={<AuthCallback />} />
-                  </Routes>
-               </MainContent>
-            </div>
-         </AppWrapper>
-      </AuthProvider>
+      <AppWrapper>
+         <Navbar />
+         <div className='flex flex-col md:flex-row w-full pt-16'>
+            <Sidebar />
+            <MainContent>
+               <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/discover' element={<Discover />} />
+                  <Route path='/search' element={<Search />} />
+                  <Route path='/about-us' element={<AboutUs />} />
+                  <Route path='/profile' element={<Profile />} />
+                  <Route path='/auth/callback' element={<AuthCallback />} />
+               </Routes>
+            </MainContent>
+         </div>
+      </AppWrapper>
    );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import { BrowserRouter } from 'react-router';
+import { AuthProvider } from './supabase/providers/AuthProvider.tsx';
 
 import App from './App.tsx';
+
 import './tailwind.css';
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
+      <AuthProvider>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </AuthProvider>
    </StrictMode>
 );
